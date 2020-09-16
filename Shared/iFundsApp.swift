50d312a8ea5fcf795e53c1dc13fbe08537022928
HeySoyl <heysoyl@icloud.com>
2020-09-16 19:34:15 +0800

    Initial Commit

---
+++ Shared/iFundsApp.swift
@@ -0,0 +1,20 @@
+//
+//  iFundsApp.swift
+//  Shared
+//
+//  Created by 刘志启 on 2020/9/16.
+//
+
+import SwiftUI
+
+@main
+struct iFundsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
